6fe9a0218e824bfc996ccfdc9775149787360630
chrizzo-hb <chrizzo@null.void>
2025-11-07 22:36:35 +0100

    Improved if/else logic of install script

--- install.sh
+++ install.sh
@@ -9,47 +9,69 @@ apply_git_settings() {
     git config push.autoSetupRemote true
 }
 
-# Dependencies
+# # Dependencies
 echo "Now installing dependencies..."
 sudo apt update && sudo apt install -y build-essential gettext docker.io
+
 mkdir -p $BASE_DIR
 cd $BASE_DIR || exit
-
 apply_git_settings
 
-# DISTRIBUTION >>>>>
-echo "Now cloning distribution..."
-git clone --recursive $DISTRIBUTION_FORK_REPO".git" $LOCAL_DISTRIBUTION_DIR_NAME
+# # DISTRIBUTION >>>>>
+if [ -d "$BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME" ]; then
+    echo "$BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME already exists - skipping git cloning."
+else
+    echo "Now cloning distribution..."
+    git clone --recursive $DISTRIBUTION_FORK_REPO".git" $LOCAL_DISTRIBUTION_DIR_NAME
+fi
 cd $BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME || exit
-
 apply_git_settings
+git fetch origin
 
 # set up our base branch
-git checkout -b $DISTRO_FORK_BRANCH
-# touch .initial-setup
-# git commit -m "Initial commit."
-# git push --set-upstream origin $DISTRO_FORK_BRANCH
+git ls-remote --exit-code --heads origin $DISTRO_FORK_BRANCH >/dev/null 2>&1
+EXIT_CODE=$?
 
-# setup our testing branch
-git checkout -b "$DISTRO_FORK_TESTING_BRANCH" "origin/$DISTRO_FORK_BRANCH"
-# git commit -m "Initial commit."
-# git push --set-upstream origin "$DISTRO_FORK_TESTING_BRANCH"
+if [[ $EXIT_CODE == '0' ]]; then
+    echo "Git branch '$DISTRO_FORK_BRANCH' exists in the remote repository"
+    git checkout $DISTRO_FORK_BRANCH
+elif [[ $EXIT_CODE == '2' ]]; then
+    echo "Git branch '$DISTRO_FORK_BRANCH' does not exist in the remote repository."
+    git checkout -b $DISTRO_FORK_BRANCH
+    touch .initial-setup
+    git commit -m "Initial commit."
+    git push --set-upstream origin $DISTRO_FORK_BRANCH
+fi
 
+# set up our base branch
+git ls-remote --exit-code --heads origin $DISTRO_FORK_TESTING_BRANCH >/dev/null 2>&1
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE == '0' ]]; then
+    echo "Git branch '$DISTRO_FORK_TESTING_BRANCH' exists in the remote repository"
+    git checkout $DISTRO_FORK_TESTING_BRANCH
+elif [[ $EXIT_CODE == '2' ]]; then
+    echo "Git branch '$DISTRO_FORK_TESTING_BRANCH' does not exist in the remote repository."
+    git checkout -b "$DISTRO_FORK_TESTING_BRANCH" "origin/$DISTRO_FORK_BRANCH"
+    git commit -m "Initial commit."
+    git push --set-upstream origin "$DISTRO_FORK_TESTING_BRANCH"
+fi
 
 # ES image creation
 echo "Now creating Docker build environment..."
 cd $BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME
 ./getPoFromWebsite.sh
-make build-docker-image # || echo "Docker image build failed. ☹️" && exit
+make build-docker-image || { echo "Docker image build failed. ☹️"; exit; }
                         # TODO: find different way to check for success (maybe docker container ls)
 
 # download all sources prior to build
 echo "Now downloading all sources..."
-make h700-source || echo "Downloading sources failed. ☹️" && exit
+make $TARGET-source || { echo "Downloading sources failed. ☹️"; exit; }
+                        # TODO: find different way to check for success
 
 # build the ES package with the new container
 echo "Now building EmulationStation package..."
-make h700-pkg PKG=knulli-emulationstation || echo "Build failed. ☹️" && exit
+make $TARGET-pkg PKG=knulli-emulationstation || { echo "Build failed. ☹️"; exit; }
 echo
 echo "Build Complete... 🔥"
 echo
@@ -57,6 +79,7 @@ echo
 
 
 # EMULATIONSTATION >>>>>
+cd $BASE_DIR
 git clone --recursive $EMULATIONSTATION_FORK_REPO $LOCAL_EMULATIONSTATION_DIR_NAME
 
 # move to base directory and set git credentials
@@ -65,18 +88,35 @@ apply_git_settings
 git fetch origin
 
 # set up our base branch
-git checkout -b $ES_FORK_GIT_BRANCH
-# git commit -m "Initial commit."
-# git push --set-upstream origin $ES_FORK_GIT_BRANCH
-#
-# setup our testing branch
-git checkout -b "$ES_FORK_GIT_TESTING_BRANCH"
-# git commit -m "Initial commit."
-# git push --set-upstream origin "$ES_FORK_GIT_TESTING_BRANCH"
-#
+git ls-remote --exit-code --heads origin $ES_FORK_GIT_BRANCH >/dev/null 2>&1
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE == '0' ]]; then
+    echo "Git branch '$ES_FORK_GIT_BRANCH' exists in the remote repository"
+    git checkout $ES_FORK_GIT_BRANCH
+elif [[ $EXIT_CODE == '2' ]]; then
+    echo "Git branch '$ES_FORK_GIT_BRANCH' does not exist in the remote repository."
+    git checkout -b $ES_FORK_GIT_BRANCH
+    touch .initial-setup
+    git commit -m "Initial commit."
+    git push --set-upstream origin $ES_FORK_GIT_BRANCH
+fi
+
+# set up our base branch
+git ls-remote --exit-code --heads origin $ES_FORK_GIT_TESTING_BRANCH >/dev/null 2>&1
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE == '0' ]]; then
+    echo "Git branch '$ES_FORK_GIT_TESTING_BRANCH' exists in the remote repository"
+    git checkout $ES_FORK_GIT_TESTING_BRANCH
+elif [[ $EXIT_CODE == '2' ]]; then
+    echo "Git branch '$ES_FORK_GIT_TESTING_BRANCH' does not exist in the remote repository."
+    git checkout -b "$ES_FORK_GIT_TESTING_BRANCH" "origin/$ES_FORK_GIT_BRANCH"
+    git commit -m "Initial commit."
+    git push --set-upstream origin "$ES_FORK_GIT_TESTING_BRANCH"
+fi
 # EMULATIONSTATION <<<<<
 
 # IN PROGRESS
 sed -i "s|^KNULLI_EMULATIONSTATION_SITE = .*|KNULLI_EMULATIONSTATION_SITE = $EMULATIONSTATION_FORK_REPO|" $BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME/package/knulli/knulli-emulationstation/knulli-emulationstation.mk
 sed -i "s|^KNULLI_EMULATIONSTATION_VERSION = .*|KNULLI_EMULATIONSTATION_VERSION = $ES_FORK_GIT_TESTING_BRANCH|" $BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME/package/knulli/knulli-emulationstation/knulli-emulationstation.mk
-#
