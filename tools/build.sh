44143e05116d68fb197d46150a99eb1d20152650
chrizzo-hb <chrizzo@null.void>
2025-11-08 02:54:36 +0100

    Automatically detect and apply the current ES dev branch

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 source ../settings.conf
 
+cd $BASE_DIR/$LOCAL_EMULATIONSTATION_DIR_NAME
+CURRENT_EMULATIONSTATION_BRANCH="$(git branch --show-current)"
+
+echo "Setting emulationstation to current development branch $CURRENT_EMULATIONSTATION_BRANCH."
+
+sed -i "s|^KNULLI_EMULATIONSTATION_SITE = .*|KNULLI_EMULATIONSTATION_SITE = $EMULATIONSTATION_FORK_REPO|" $BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME/package/knulli/knulli-emulationstation/knulli-emulationstation.mk
+sed -i "s|^KNULLI_EMULATIONSTATION_VERSION = .*|KNULLI_EMULATIONSTATION_VERSION = $CURRENT_EMULATIONSTATION_BRANCH|" $BASE_DIR/$LOCAL_DISTRIBUTION_DIR_NAME/package/knulli/knulli-emulationstation/knulli-emulationstation.mk
+
 PWD=$(pwd)
-rm -rf "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/dl/batocera-emulationstation/
-rm -rf "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/output/"$TARGET"/build/batocera-emulationstation-"$FEATURE_BRANCH_NAME"_"$TEST_BRANCH_SUFFIX"
+rm -rf "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/dl/knulli-emulationstation/
+rm -rf "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/output/"$TARGET"/build/knulli-emulationstation-"$CURRENT_EMULATIONSTATION_BRANCH"
 cd "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"
-make "$TARGET"-pkg PKG=batocera-emulationstation
+make "$TARGET"-pkg PKG=knulli-emulationstation
 cd $PWD
 echo
 echo "Build complete. 🔥"
