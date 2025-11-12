000ec2f79c794967671a9ae57e6abe2cebf42180
chrizzo-hb <chrizzo@null.void>
2025-11-12 01:07:46 +0100

    Fixed deployment script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 source "../settings.conf"
 
+cd $BASE_DIR/$LOCAL_EMULATIONSTATION_DIR_NAME
+CURRENT_EMULATIONSTATION_BRANCH="$(git branch --show-current)"
+
 PWD=$(pwd)
 echo "Beginning Deployment. 🤞"
 ssh $DEVICE_LOGIN "mkdir -p ~/backup && cp /usr/bin/emulationstation ~/backup/emulationstation.lastversion && /etc/init.d/S31emulationstation stop"
-scp "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/output/"$TARGET"/build/batocera-emulationstation-"$FEATURE_BRANCH_NAME"_"$TEST_BRANCH_SUFFIX"/emulationstation $DEVICE_LOGIN:~/backup/emulationstation.newversion
+scp "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/output/"$TARGET"/build/knulli-emulationstation-"$CURRENT_EMULATIONSTATION_BRANCH"/emulationstation $DEVICE_LOGIN:~/backup/emulationstation.newversion
 ssh "$DEVICE_LOGIN" "cp ~/backup/emulationstation.newversion /usr/bin/emulationstation && batocera-save-overlay && reboot && exit"
 cd "$PWD"
 echo
