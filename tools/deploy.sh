c5e9a341d7f766ed7949c3b7fb2dce274c258f42
chrizzo-hb <chrizzo@null.void>
2025-11-16 21:30:41 +0100

    Make deployment script work for old and new naming pattern

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -8,7 +8,7 @@ PWD=$(pwd)
 echo "Beginning Deployment. 🤞"
 ssh $DEVICE_LOGIN "mkdir -p ~/backup && cp /usr/bin/emulationstation ~/backup/emulationstation.lastversion && /etc/init.d/S31emulationstation stop"
 scp "$BASE_DIR"/"$LOCAL_DISTRIBUTION_DIR_NAME"/output/"$TARGET"/build/knulli-emulationstation-"$CURRENT_EMULATIONSTATION_BRANCH"/emulationstation $DEVICE_LOGIN:~/backup/emulationstation.newversion
-ssh "$DEVICE_LOGIN" "cp ~/backup/emulationstation.newversion /usr/bin/emulationstation && batocera-save-overlay && reboot && exit"
+ssh "$DEVICE_LOGIN" "cp ~/backup/emulationstation.newversion /usr/bin/emulationstation && batocera-save-overlay ; knulli-save-overlay ; reboot && exit"
 cd "$PWD"
 echo
 echo "Deployment complete. 🔥"
